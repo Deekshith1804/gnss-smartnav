04c02f01fe9403df1b547803a3221b82bfbd2620
Deekshith Goud <deekshithgoud@Deekshiths-MacBook-Air.local>
2025-07-03 14:48:57 +0530

    Fix: Use process.env.PORT for Render deployment

--- digipin/server.js
+++ digipin/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-console.log(`DIGIPIN API is running and API docs can be found at at http://localhost:${PORT}/api-docs`);
+  console.log(`DIGIPIN API is running and API docs can be found at http://localhost:${PORT}/api-docs`);
 });
+
