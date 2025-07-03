a55ef022abb5b527bd7140aa306816610dfc753b
Deekshith Goud <deekshithgoud@Deekshiths-MacBook-Air.local>
2025-07-03 15:14:26 +0530

    Fix port binding and add Procfile for Render

--- digipin/server.js
+++ digipin/server.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
 const app = require('./src/app');
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`DIGIPIN API is running and API docs can be found at http://localhost:${PORT}/api-docs`);
 });
-
